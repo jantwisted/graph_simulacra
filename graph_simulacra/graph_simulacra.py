1e9a7ef46740dde71e088c593db0b39ce8636531
Janith Perera <janith@member.fsf.org>
2019-03-24 01:09:51 +0100

    ranking added

--- graph_simulacra/graph_simulacra.py
+++ graph_simulacra/graph_simulacra.py
@@ -6,31 +6,56 @@ import matplotlib.pyplot as plt
 
 """Main module."""
 
-def input_matrix(x = 1, y = 1):
+def input_matrix(v = 1):
     matrix_array = []
-    print("Insert the matrix, separated by (,)")
-    for i in range(y):
+    print("Insert the |V| × |V| matrix, separated by (,) and RETURN")
+    for i in range(v):
         row = input()
         matrix_array.append(list(map(int, row.split(','))))
         i += 1
     return matrix_array
 
 def matrix_construct():
-    x = int(input("Insert x:"))
-    y = int(input("Insert y:"))
-    return (x, y)
+    v = int(input("Insert # of Vertices:\n"))
+    return v
 
 def get_matrix():
-    (x, y) = matrix_construct()
-    return input_matrix(x, y)
+    v = matrix_construct()
+    return input_matrix(v)
+
+get_label_map = lambda: {v: k for v, k in enumerate(list(map(str, input("Insert labels, separated by (,)\n").split(','))))}
+get_vertex_rank = lambda: {v: k for v, k in enumerate(list(map(str, input("Insert vertex rank, separated by (,)\n").split(','))))}
 
 def draw_graph(matrix_array):
     """draws the graph based on adjacency matrix"""
+
+    # Initialize the Graph
     G = nx.Graph()
     adjacency_matrix = np.array(matrix_array)
     G = nx.from_numpy_matrix(adjacency_matrix)
-    nx.draw(G, cmap = plt.get_cmap('jet'), with_labels=True)
+
+    # Setting up the rank
+    rank_map = get_vertex_rank()
+    nx.set_node_attributes(G, rank_map, 'rank')
+
+    # Setting up the labels
+    labels_map = get_label_map()
+    G = nx.relabel_nodes(G, labels_map)
+
+    # Draw Graph
+    g_nodes = nx.spring_layout(G)
+    plt.figure()
+    nx.draw(G, g_nodes, cmap = plt.get_cmap('jet'), with_labels=True)
+    g_node_attributes = {}
+    for node, coordinates in g_nodes.items():
+        g_node_attributes[node] = (coordinates[0], coordinates[1] - 0.08)
+    node_attributes = nx.get_node_attributes(G, 'rank')
+    customer_node_attributes = {}
+    for node, attr in node_attributes.items():
+        customer_node_attributes[node] = "r:" + attr
+    nx.draw_networkx_labels(G, g_node_attributes, labels=customer_node_attributes)
     plt.show()
+
 
 if __name__=='__main__':
     """main method"""
